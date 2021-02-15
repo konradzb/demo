742587c37a43e79375bfc86d2753e4474c475a09
Jerzy9 <zbylutkonrad@gmail.com>
2021-02-15 21:20:28 +0100

    update

--- src/main/java/com/example/demo/security/AplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/AplicationSecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import java.rmi.StubNotFoundException;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
